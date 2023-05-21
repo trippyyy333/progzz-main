7fe5cc61dc2aef2a71f68aad345fb023f725aa8d
trippyyy333 <75495653+trippyyy333@users.noreply.github.com>
2023-05-21 07:12:16 -0400

    dsds

---
+++ src/runtime.js
@@ -0,0 +1,7 @@
+'use strict';
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('/src/react-refresh/cjs/react-refresh-runtime.development.js');
+} else {
+  module.exports = require('/src/react-refresh/cjs/react-refresh-runtime.development.js');
+}
